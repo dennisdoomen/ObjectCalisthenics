6490e9ed3be630f3b6ba5e7f5ed73bfc76997927
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2013-05-12 14:18:24 +0200

    Refactored all nested control structures

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 
@@ -29,75 +30,108 @@ namespace GildedRose
             List<Item> items = list;
             foreach (var item in items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
+                ProcessItem(item);
+            }
+        }
 
-                        if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
+        private static void ProcessItem(Item item)
+        {
+            if ((item.Name != "Aged Brie") && (item.Name != "Backstage passes to a TAFKAL80ETC concert"))
+            {
+                ReduceQualityUnlessNameIsSulfuras(item);
+            }
+            else
+            {
+                HandleInsufficientQuality(item);
+            }
 
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
+            if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+            {
+                item.SellIn = item.SellIn - 1;
+            }
 
-                if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+            if (item.SellIn < 0)
+            {
+                HandleInsufficientSellIn(item);
+            }
+        }
 
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
+        private static void ReduceQualityUnlessNameIsSulfuras(Item item)
+        {
+            if ((item.Quality > 0) && (item.Name != "Sulfuras, Hand of Ragnaros"))
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+
+        private static void HandleInsufficientQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                HandleBackstagePasses(item);
+            }
+        }
+
+        private static void HandleBackstagePasses(Item item)
+        {
+            if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                UpdateQualityBasedOnQualityAndSellIn(item);
+            }
+        }
+
+        private static void UpdateQualityBasedOnQualityAndSellIn(Item item)
+        {
+            if ((item.SellIn < 11) && (item.Quality < 50))
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            if ((item.SellIn < 6) && (item.Quality < 50))
+            {
+                item.Quality = item.Quality + 1;
+            }
+        }
+
+        private static void HandleInsufficientSellIn(Item item)
+        {
+            if (item.Name != "Aged Brie")
+            {
+                HandleItemsThatAreNotAgedBrie(item);
+            }
+            else
+            {
+                HandleOtherItems(item);
+            }
+        }
+
+        private static void HandleItemsThatAreNotAgedBrie(Item item)
+        {
+            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                IfNotSulfurasAndQualityIsSufficient(item);
+            }
+            else
+            {
+                item.Quality = item.Quality - item.Quality;
+            }
+        }
+
+        private static void IfNotSulfurasAndQualityIsSufficient(Item item)
+        {
+            if ((item.Quality > 0) && (item.Name != "Sulfuras, Hand of Ragnaros"))
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+
+        private static void HandleOtherItems(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
             }
         }
     }
